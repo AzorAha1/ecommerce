12a622ebe169426b639cdbfddd7faebd2745924d
Azor <wizfaiz@icloud.com>
2023-03-08 15:13:34 +0100

    new images added and homepage search bar and app bar added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -13,15 +13,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text('My Shop',style: TextStyle(color: Colors.white),),
-        actions: [
-          InkWell(
-            child:LineIcon(LineIcons.shoppingBag),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            'My Shop',
+            style: TextStyle(color: Colors.white),
           ),
-        ],
-      ),
-    );
+          actions: [
+            InkWell(
+              child: Icon(Icons.shopping_cart),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, right: 5, left: 20),
+              child: InkWell(
+                child: Text('Orders'),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, right: 40, left: 20),
+              child: InkWell(
+                child: Text('Account'),
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Row(
+             
+              children: [
+                SizedBox(width: 100,),
+                InkWell(child: Icon(Icons.search),),
+                Expanded(child: Padding(
+                  padding: const EdgeInsets.only(right: 100, left: 10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search Items'
+                    ),
+                  ),
+                ))
+              ],
+            ),
+            
+          ],
+        ),
+        
+        );
   }
 }
